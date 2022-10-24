8761a255c349dc2cb6e878706f088eaf162ffdb4
Joairazabal <95320625+Joairazabal@users.noreply.github.com>
2022-10-24 10:24:00 -0300

    ready

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("WHATS IS YOUR NAME? ");
+string nombre= Console.ReadLine();
+Console.WriteLine("hi " + nombre);
+
+Console.WriteLine("what time is it?");
+string hora= Console.ReadLine();
+string day = "2022,10,24";
+Console.WriteLine("Hoy es " + day +" y son las " + hora );
+
